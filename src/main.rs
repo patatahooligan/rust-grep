d796c5b86e26fc23bfe6656c66018ed03ffb238a
patatahooligan <filwn.oikonomou@gmail.com>
2020-03-22 00:51:28 +0200

    Print errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Error when parsing arguments: {}", err);
+        eprintln!("Error when parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(error) = grep::run(config) {
-        println!("{}", error);
+        eprintln!("{}", error);
         process::exit(1);
     }
 }
